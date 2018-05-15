0d19871a8277562c8667982bfa41a0704f8813d1
patryk makuch <makuchpatryk@gmail.com>
2018-05-15 22:24:44 +0100

    Add first models

--- polls/polls/settings.py
+++ polls/polls/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pollsapp.apps.PollsappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'polls.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'polls_app',
+        'USER': 'polls',
+        'PASSWORD': 'polls',
+        'HOST': 'localhost',
     }
 }
 
